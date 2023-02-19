098c06ffbb0bf863f1b39a18bfdb63faf8e0bd1c
Qixin <mersenyip0130@gmail.com>
2023-02-19 03:44:48 +0000

    add internal server error handler

--- uwconnect_core/main/handler/__init__.py
+++ uwconnect_core/main/handler/__init__.py
@@ -2,3 +2,4 @@ from uwconnect_core.main.handler.error_handler import *
 from uwconnect_core.main import app
 
 app.register_error_handler(BadRequest, handle_bad_request)
+app.register_error_handler(InternalServerError, handle_internal_server_error)
--- uwconnect_core/main/handler/error_handler.py
+++ uwconnect_core/main/handler/error_handler.py
@@ -1,5 +1,11 @@
 from werkzeug.exceptions import BadRequest
+from werkzeug.exceptions import InternalServerError
 
 
 def handle_bad_request(e: BadRequest):
     return { "message": e.description }, 400
+
+
+def handle_internal_server_error(e: InternalServerError):
+    return { "message": "unexpected error" }, 500
+
