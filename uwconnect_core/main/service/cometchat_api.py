54965a228f55fc998429be563fe8b09e6356a708
Haopu Chen <dealexce@sina.com>
2023-03-16 22:42:29 +0000

    [fix] change app to current_app

--- uwconnect_core/main/service/cometchat_api.py
+++ uwconnect_core/main/service/cometchat_api.py
@@ -1,23 +1,23 @@
 import requests
-from uwconnect_core.main import app
+from flask import current_app
 
 def get_friends(uid, friend_uid):
-    url = f"https://{app.config['COMETCHAT_APP_ID']}.api-us.cometchat.io/v3/users/{uid}/friends"
+    url = f"https://{current_app.config['COMETCHAT_APP_ID']}.api-us.cometchat.io/v3/users/{uid}/friends"
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
-        "apikey": app.config["COMETCHAT_API_KEY"]
+        "apikey": current_app.config["COMETCHAT_API_KEY"]
     }
     if friend_uid:
         url += f"?searchKey={friend_uid}"
     return requests.get(url, headers=headers).json().get("data")
 
 def add_friends(uid, friend_uid):
-    url = f"https://{app.config['COMETCHAT_APP_ID']}.api-us.cometchat.io/v3/users/{uid}/friends"
+    url = f"https://{current_app.config['COMETCHAT_APP_ID']}.api-us.cometchat.io/v3/users/{uid}/friends"
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
-        "apikey": app.config["COMETCHAT_API_KEY"]
+        "apikey": current_app.config["COMETCHAT_API_KEY"]
     }
     payload = {"accepted": [friend_uid]}
     response = requests.post(url, json=payload, headers=headers)
