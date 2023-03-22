9051c891aa6019201fb042a5e0214f6e62451b18
vinesmsuic <34955859+vinesmsuic@users.noreply.github.com>
2023-03-22 17:07:29 -0400

    adding recommendation backend api

--- uwconnect_core/main/service/user_service.py
+++ uwconnect_core/main/service/user_service.py
@@ -29,17 +29,16 @@ def check_login(func):
 
 
 def cometchat_create_user(uid, username):
-    # url = f"https://{current_app.config['COMETCHAT_APP_ID']}.api-us.cometchat.io/v3/users"
+    url = f"https://{current_app.config['COMETCHAT_APP_ID']}.api-us.cometchat.io/v3/users"
 
-    # payload = {
-    #             "uid": uid,
-    #             "name": username
-    #         }
-    # headers = {
-    #     "accept": "application/json",
-    #     "content-type": "application/json",
-    #     "apikey": current_app.config["COMETCHAT_API_KEY"]
-    # }
+    payload = {
+                "uid": uid,
+                "name": username
+            }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "apikey": current_app.config["COMETCHAT_API_KEY"]
+    }
 
-    # requests.post(url, json=payload, headers=headers)
-    return 
+    requests.post(url, json=payload, headers=headers)
