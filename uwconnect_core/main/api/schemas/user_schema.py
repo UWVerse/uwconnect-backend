9705c9b3584774bf5453ed9cd1d4719f18a4c9a8
Qixin <mersenyip0130@gmail.com>
2023-04-05 23:20:51 +0000

    friend list api

--- uwconnect_core/main/api/schemas/user_schema.py
+++ uwconnect_core/main/api/schemas/user_schema.py
@@ -51,3 +51,8 @@ class UserListSchema(flaskMarshal.Schema):
         ordered = True
     users = flaskMarshal.List(flaskMarshal.Nested(UserSchema))
     total = flaskMarshal.Integer()
+
+class CometchatUidListSchema(flaskMarshal.Schema):
+    class Meta:
+        ordered = True
+    emails = flaskMarshal.List(flaskMarshal.String())
